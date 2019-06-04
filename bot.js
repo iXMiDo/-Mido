3bc08ec49e198d4bc175be2c8c5b1d7eaf1ca134
iXMiDo <41427808+iXMiDo@users.noreply.github.com>
2019-06-04 10:16:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+
+
+
+
+
+
+
 
 client.on('ready', () => {
    console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`Credit Farmm - Script By : Stewart `);
    console.log(`----------------`);
    console.log(`Loadinng`);
    console.log(`Loadinng.`);
@@ -16,49 +24,65 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === '!daily'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-مبلغ'){
+    if(message.content === '!credits'){
         message.channel.send('#credits')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-راتب'){
+    if(message.content === '!daily'){
         message.channel.send('#daily')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-مبلغ'){
+    if(message.content === '!credits'){
         message.channel.send('#credits')
     }
 });
 
 client2.on('message', message => {
-    if(message.content === '-ريب'){
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '!daily'){
+        message.channel.send('#daily')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '!credits'){
         message.channel.send('#credits')
     }
 });
 
-
+client3.on('message', message => {
+    if(message.content === '!rep'){
+        message.channel.send('#rep @Stewart#0005 ')
+    }
+});
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -72,7 +96,21 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Fram , Credit By stewart , Farm Credit By stewart, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج SASFHASHFASHFJASHJFHASJFHASJHFJAHJKSFHAKJSHFKAJSHFKJAS**[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -83,6 +121,6 @@ if (message.content === '!spam') {
 
 
 
-
-client.login(process.env.TOKEN);// لا تغير فيها شيء
-client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client.login(process.env.BOT_TOKEN);
+client2.login(process.env.BOT_TOKEN2);
+client3.login(process.env.BOT_TOKEN3);
